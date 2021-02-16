f75ffc53b6e92798379641bb7d598bc3a4dd60ea
scullchr <christopher.scully@workweargroup.com.au>
2021-02-17 10:19:31 +1100

    initial next-js plus tailwind

---
+++ src/components/layout/index.js
@@ -0,0 +1,13 @@
+import Header from "../header";
+
+
+function Layout({children}) {
+    return (
+        <div>
+        <Header/>
+        {children}
+        </div>
+    )
+}
+
+export default Layout;
